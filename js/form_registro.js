d59af857006ed11ec18ffacac85b76de633f159b
JoaquinVenegas <jo.venegasm@duocuc.cl>
2024-03-20 16:13:16 -0300

    cambios rama
    a

---
+++ js/form_registro.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    
+    $('#IngresoForm').submit(function(e){
+        //preventDefault() evita que el formulario se envíe por defecto, ya que primero validaremos.
+        e.preventDefault();
+        var nombre_usuario = $("#in_usuario").val();
+        var email = $("#in_correo").val();
+        var password = $("#in_password").val();
+        var pasword2 = $("#in_password2").val();
+        var fecha_nac = $("#in_fecha").val();
+    
+        // Validación campos vacíos
+        if (nombre_usuario == "" || email == "" ||
+            password == "" || pasword2 == "" || fecha_nac == "" )
+        {
+            alert("Debe ingresar campos obligatorios");
+            return false;
+        }
+
+        // Validación que las dos contraseñas sean iguales
+        if (password != pasword2)
+        {
+            alert("Contraseñas deben ser iguales entre si");
+            return false;
+        }
+
+        // Validación de rango de longitud de contraseña entre 6 y 18 caracteres
+        if (password.length < 6 || password.length > 18)
+        {
+            alert("Contraseña debe tener una longitud entre 6 y 18 caracteres");
+            return false;
+        }
+
+        var passwordPattern = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$";
+        if (!passwordPattern.test(password)) {
+            alert("La contraseña debe contener al menos un número y una letra en mayúscula y tener entre 6 y 18 caracteres");
+            return false;
+        }
+
+        // Validación para que la persona no sea a menor de 13 años
+        var anios = calcular_anios(fecha_nac);
+        console.log("años obtenidos: " + anios);
+
+        if (anios < 13){
+            alert("Persona no puede ser menor de 13 años");
+            return false;
+        }
+
+    });
+
+    function calcular_anios(fecha_nac){
+        var today = new Date();
+        var birthDate = new Date(fecha_nac);
+        var age = today.getFullYear() - birthDate.getFullYear();
+
+        if (today.getMonth() < birthDate.getMonth() || (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        return age;
+    }
+
+});
